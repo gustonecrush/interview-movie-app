3bcff5649597c936ada9ab8fc96635faf7123a53
gustonecrush <codeboxezz@gmail.com>
2023-02-08 09:25:42 +0700

    added new migration and model category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,13 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Movie extends Model
+class Category extends Model
 {
     use HasFactory;
-
-    protected $guarded = [];
     
-    public function category() {
-        return $this->belongsTo(Category::class);
+    public function movies() {
+        return $this->hasMany(Movie::class);
     }
 }
