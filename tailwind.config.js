befc6448140f46ae878bcc49faf2e31e34481a3a
gustonecrush <codeboxezz@gmail.com>
2023-02-08 09:31:30 +0700

    config tailwindcss setted up theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,25 @@ module.exports = {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '1rem',
+                md: '2rem',
+                lg: '50px',
+                xl: '120px'
+            }
+        },
+        fontFamily: {
+            poppins: 'Poppins, sans-serif'
+        },
+        extend: {
+            colors: {
+                "stream-dark": '#0A071B',
+                "stream-gray": '#8C87A2',
+                "softpur": '#3D3762',
+            }
+        }
     },
     plugins: [],
 };
